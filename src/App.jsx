30cd7beeb2f67b9b6dbb015cc4e73d4901e78bbd
James Murphy <jamesm8228@gmail.com>
2025-04-09 18:21:30 -0400

    Add components foler and get gfirst animations for text on land page ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import "./index.css";
+import { LoadingScreen } from "./components/LoadingScreen";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [isLoaded, setIsLoaded] = useState(false);
 
-  return <></>;
+  return (
+    <>{!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}</>
+  );
 }
 
 export default App;
