eec708c1df66387f29e1b8800f3b0f3d80da09d4
James Murphy <jamesm8228@gmail.com>
2025-04-10 09:12:50 -0400

    Add styling changes to display intial message on load of page, and also add Navbar template for site

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,21 @@ import { useState } from "react";
 import "./App.css";
 import "./index.css";
 import { LoadingScreen } from "./components/LoadingScreen";
+import { Navbar } from "./components/Navbar";
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
 
   return (
-    <>{!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}</>
+    <>
+      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
+      <div
+        className={`${
+          isLoaded ? "opactity-100" : "opacity-0"
+        } min-h-screen transition-opacity duration-700 bg-black text-gray-100`}
+      ></div>
+      <Navbar />
+    </>
   );
 }
 
