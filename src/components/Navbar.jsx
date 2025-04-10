abfade409529c44f36665d2aa47f2001bcd8a853
James Murphy <jamesm8228@gmail.com>
2025-04-10 09:16:18 -0400

    Add text change for misspelling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ export const Navbar = () => {
         <div className="flex justify-between items-center h-16">
           <a href="#home" className="font-mono text-xl font-bold text-white">
             {" "}
-            Logo <span className="text-blue-500">.Placeholder</span>
+            Logo<span className="text-blue-500">.Placeholder</span>
           </a>
           <div className="w-7 h-5 relative cursor-pointer z-40 md:hidden">
             &#9776;
@@ -28,7 +28,7 @@ export const Navbar = () => {
               href="#projects"
               className="text-gray-300 hover:text-white transition-colors"
             >
-              Contact
+              Projects
             </a>
             <a
               href="#contact"
