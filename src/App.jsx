bae749df8d6f052e9900d1a074ccc20d4cd0d588
James Murphy <jamesm8228@gmail.com>
2025-04-12 10:52:14 -0400

    Add mobile menu for screen adjustment when screen size has changed

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,11 @@ import "./App.css";
 import "./index.css";
 import { LoadingScreen } from "./components/LoadingScreen";
 import { Navbar } from "./components/Navbar";
+import { MobileMenu } from "./components/MobileMenu";
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   return (
     <>
@@ -15,7 +17,8 @@ function App() {
           isLoaded ? "opactity-100" : "opacity-0"
         } min-h-screen transition-opacity duration-700 bg-black text-gray-100`}
       ></div>
-      <Navbar />
+      <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
     </>
   );
 }
