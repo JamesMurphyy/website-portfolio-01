1d21fbdb3412b8cc334b4e74ec3aa78ca75bf1d8
James Murphy <jamesm8228@gmail.com>
2025-04-15 10:28:55 -0400

    add contact section and also add stying changes to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,13 +5,13 @@ export const Navbar = ({ menuOpen, setMenuOpen }) => {
     document.body.style.overflow = menuOpen ? "hidden" : "";
   }, [menuOpen]);
   return (
-    <nav className="fixed top-0 w-full z-40 bg-[rgba(10,10,10,0.8)] backdrop-blur-lg border-b border-white/10 shadow-lg">
+    <nav className="fixed top-0 w-full z-40 bg-[rgb(255,255,255)] backdrop-blur-lg border-b border-white/10 shadow-lg">
       {" "}
       <div className="max-w-5xl mx-auto px-4">
         <div className="flex justify-between items-center h-16">
-          <a href="#home" className="font-mono text-xl font-bold text-white">
+          <a href="#home" className="font-sans text-3xl font-bold text-black">
             {" "}
-            Logo<span className="text-blue-500">.Placeholder</span>
+            Debby <span className="text-black">Murphy</span>
           </a>
           <div
             className="w-7 h-5 relative cursor-pointer z-40 md:hidden"
@@ -22,27 +22,37 @@ export const Navbar = ({ menuOpen, setMenuOpen }) => {
           <div className="hidden md:flex items-center space-x-8">
             <a
               href="#home"
-              className="text-gray-300 hover:text-white transition-colors"
+              className="text-gray-400 hover:text-black transition-colors delay-100 duration-200 ease-in-out"
             >
               Home
             </a>
             <a
               href="#about"
-              className="text-gray-300 hover:text-white transition-colors"
+              className="text-gray-400 hover:text-black transition-colors delay-100 duration-200 ease-in-out"
             >
               About
             </a>
             <a
               href="#projects"
-              className="text-gray-300 hover:text-white transition-colors"
+              className="text-gray-400 hover:text-black transition-colors delay-100 duration-200 ease-in-out"
             >
               Projects
             </a>
             <a
               href="#contact"
-              className="text-gray-300 hover:text-white transition-colors"
+              className="text-gray-400 hover:text-black transition-colors delay-100 duration-200 ease-in-out"
             >
               Contact
+            </a>
+            <a className="text-black ">
+              <div>
+                <span className="">call</span>
+                999 - 999 - 1000
+              </div>
+              <div>
+                <span class="">alternate_email</span>
+                999 - 999 - 1000
+              </div>
             </a>
           </div>
         </div>
