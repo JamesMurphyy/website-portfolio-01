335d625e1202fa6a8ea90a67474e4d681864134f
James Murphy <jamesm8228@gmail.com>
2025-04-15 09:29:45 -0400

    Add about section with some changes to styling of background

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { LoadingScreen } from "./components/LoadingScreen";
 import { Navbar } from "./components/Navbar";
 import { MobileMenu } from "./components/MobileMenu";
 import { Home } from "./components/sections/Home";
+import { About } from "./components/sections/About";
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
@@ -16,11 +17,12 @@ function App() {
       <div
         className={`${
           isLoaded ? "opactity-100" : "opacity-0"
-        } min-h-screen transition-opacity duration-700 bg-black text-gray-100`}
+        } min-h-screen transition-opacity duration-700 bg-[#494949] text-gray-100`}
       >
         <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
         <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
         <Home />
+        <About />
       </div>
     </>
   );
