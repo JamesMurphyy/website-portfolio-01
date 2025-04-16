db08e7aef2da1a8216adb92e53c34debbcaedba4
James Murphy <jamesm8228@gmail.com>
2025-04-16 10:12:24 -0400

    Add text colour changes to title

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -16,8 +16,8 @@ export const Home = () => {
         <div className="text-center z-10 px-4 content">
           {/* added 'content' to classname for video bg */}
 
-          <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent leading-right">
-            Hi, I'm Developer
+          <h1 className="text-3xl md:text-5xl font-bold mb-6 pb-2 bg-gradient-to-r from-white to-zinc-400 bg-clip-text text-transparent leading-right">
+            Debby Murphy Realestate
           </h1>
 
           <p className="tex-gray-400 text-lg mb-8 max-w-lg mx-auto">
@@ -26,15 +26,15 @@ export const Home = () => {
           <div className="flex justify-center space-x-4">
             <a
               href="#projects"
-              className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.4)]"
+              className="bg-gray-200/20 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.4)]"
             >
               View Projects
             </a>
 
             <a
               href="#contact"
-              className="border border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200 
-             hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.2)] hover:bg-blue-500/10"
+              className="border border-black/50 text-white py-3 px-6 rounded font-medium transition-all duration-200 
+             hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.2)] hover:bg-gray-200/10"
             >
               Contact Me
             </a>
