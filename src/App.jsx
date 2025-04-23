1918576930e4278992a8eff6ac875d221ca61172
James Murphy <jamesm8228@gmail.com>
2025-04-23 09:24:25 -0400

    Add changes to background, makiing it look nicer with tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -16,11 +16,17 @@ function App() {
   return (
     <>
       {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
-      <div
+      {/* <div
         className={`${
           isLoaded ? "opactity-100" : "opacity-0"
         } min-h-screen transition-opacity duration-1000 bg-[#cbcbcb] text-gray-100`}
+      > */}
+      <div
+        className={`${
+          isLoaded ? "opactity-100" : "opacity-0"
+        } relative h-full w-full bg-white`}
       >
+        <div className="absolute h-full w-full bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px] [mask-image:radial-gradient(ellipse_50%_50%_at_50%_50%,#000_70%,transparent_100%)]"></div>
         <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
         <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
         <Home />
--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -31,7 +31,7 @@ export const Home = () => {
               href="#projects"
               className="bg-gray-200/20 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.4)]"
             >
-              View Projects
+              View Reviews
             </a>
 
             <a
